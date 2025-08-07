dbc0b976a3a8487de854cd4fca4bc02b77d57aaf
Rafals <mb190t@gmail.com>
2025-08-07 14:53:36 +0200

    origin

--- src/com/codegym/task/task22/task2213/GamePiece.java
+++ src/com/codegym/task/task22/task2213/GamePiece.java
@@ -21,4 +21,15 @@ public class GamePiece {
         this.y = y;
         this.matrix = matrix;
     }
+
+    public void left(){}
+    public void right(){}
+    public void down(){}
+    public void up(){}
+    public void rotate(){}
+    public void downMaximum(){}
+    public boolean isCurrentPositionAvailable(){
+        return true;
+    }
+    public void land(){}
 }
