04e81d87604e2e28b741b9a99d7a84fd4709e227
Rafals <mb190t@gmail.com>
2025-08-10 23:46:41 +0200

    origin

--- src/com/codegym/task/task25/task2515/Spaceship.java
+++ src/com/codegym/task/task25/task2515/Spaceship.java
@@ -3,8 +3,6 @@ package com.codegym.task.task25.task2515;
 public class Spaceship extends BaseObject {
     private double dx = 0;
 
-
-
     public Spaceship(double x, double y) {
         super(x, y, 3);
     }
@@ -15,5 +13,21 @@ public class Spaceship extends BaseObject {
 
     public void moveRight() {
         dx = 1;
+    }
+
+    @Override
+    public void move() {
+        x = x + dx;
+        checkBorders(radius, Space.game.getWidth() - radius + 1, 1, Space.game.getHeight() + 1);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    public void fire() {
+        Space.game.getRockets().add(new Rocket(x - 2, y));
+        Space.game.getRockets().add(new Rocket(x + 2, y));
     }
 }
