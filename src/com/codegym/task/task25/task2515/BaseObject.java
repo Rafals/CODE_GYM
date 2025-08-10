a0dedc6b92199c8275e41627886e9e5f676fa0b9
Rafals <mb190t@gmail.com>
2025-08-10 16:16:12 +0200

    origin

--- src/com/codegym/task/task25/task2515/BaseObject.java
+++ src/com/codegym/task/task25/task2515/BaseObject.java
@@ -1,8 +1,41 @@
 package com.codegym.task.task25.task2515;
 
 public abstract class BaseObject {
-    private double x,y,radius;
+
+    private double x;
+    private double y;
+    private double radius;
+
     private boolean isAlive;
+
+    public BaseObject(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.isAlive = true;
+    }
+
+    public void draw() {
+    }
+
+    public void move() {
+    }
+
+    public void die() {
+        isAlive = false;
+    }
+
+    public boolean intersects(BaseObject o) {
+        double dx = x - o.x;
+        double dy = y - o.y;
+        double distance = Math.sqrt(dx * dx + dy * dy);
+        double distance2 = Math.max(radius, o.radius);
+        return distance <= distance2;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
 
     public double getX() {
         return x;
@@ -27,16 +60,4 @@ public abstract class BaseObject {
     public void setRadius(double radius) {
         this.radius = radius;
     }
-
-    public boolean isAlive() {
-        return isAlive;
-    }
-
-    public BaseObject(double x, double y, double radius) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        isAlive = true;
-    }
-
 }
