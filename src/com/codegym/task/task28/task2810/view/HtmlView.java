2db0b2469db13a36ddde5b4b38f54b7dfed404b6
Rafals <mb190t@gmail.com>
2025-09-05 21:59:45 +0200

    origin

--- src/com/codegym/task/task28/task2810/view/HtmlView.java
+++ src/com/codegym/task/task28/task2810/view/HtmlView.java
@@ -7,15 +7,34 @@ import java.util.List;
 
 public class HtmlView implements View {
 
+    private final String filePath = "./4.JavaCollections/src/" + this.getClass().getPackage().getName().replaceAll("[.]", "/") + "/jobPostings.html";
+
     private Controller controller;
 
     @Override
     public void update(List<JobPosting> vacancies) {
-
+        try {
+            String newContent = getUpdatedFileContents(vacancies);
+            updateFile(newContent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void setController(Controller controller) {
         this.controller = controller;
+    }
+
+    public void emulateCitySelection() {
+        controller.onCitySelected("San Francisco");
+    }
+
+    private String getUpdatedFileContents(List<JobPosting> vacancies) {
+        return null;
+    }
+
+    private void updateFile(String content) {
+
     }
 }
